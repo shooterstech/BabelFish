d596d941f8f719ce6f32f2f3f1a46b817c1bf577
Erik Anderson <erik@shooterstech.net>
2022-01-11 10:00:42 -0500

    Added enum for the Api Stage

--- APIClient.cs
+++ APIClient.cs
@@ -1,12 +1,26 @@
-﻿using BabelFish.Requests;
+﻿using System.ComponentModel;
+using System.Runtime.Serialization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using BabelFish.Requests;
 
 namespace BabelFish {
-    public abstract class APIClient  {
+    public abstract class APIClient {
+
+        [JsonConverter( typeof( StringEnumConverter ) )]
+        public enum APIStage {
+            [Description( "alpha" )] [EnumMember( Value = "alpha" )] ALPHA,
+            [Description( "beta" )] [EnumMember( Value = "beta" )] BETA,
+            [Description( "production" )] [EnumMember( Value = "production" )] PRODUCTION
+        }
 
         public APIClient( string xapikey ) {
             this.XApiKey = xapikey;
+            ApiStage = APIStage.PRODUCTION;
         }
 
         public string XApiKey { get; set; }
+
+        public APIStage ApiStage { get; set; }
     }
 }
