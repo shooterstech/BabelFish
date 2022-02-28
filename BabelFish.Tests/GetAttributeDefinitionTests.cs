30ccc59cda62d11e4dcf99c3ee85210861e3b017
Adam Swanger <adam@shooterstech.net>
2022-02-28 07:35:43 -0500

    Add caching logic memory development notes. Add UnitTests for existing Definition calls.

--- BabelFish.Tests/GetAttributeDefinitionTests.cs
+++ BabelFish.Tests/GetAttributeDefinitionTests.cs
@@ -10,31 +10,208 @@ using BabelFish.DataModel.Definitions;
 
 namespace BabelFish.Tests {
     [TestClass]
-    public class GetAttributeDefinitionTests {
+    public class GetAttributeDefinitionTests
+    {
+        private SetName setName;
+        private static string xApiKey = "wjM7eCb75aa3Okxj4FliXLY0VjHidoE2ei18pdg1";
+        private readonly DefinitionAPIClient _client = new DefinitionAPIClient(xApiKey);
 
         [TestMethod]
         public void GetAttributeAirRifleType() {
-            string xApiKey = "wjM7eCb75aa3Okxj4FliXLY0VjHidoE2ei18pdg1";
 
             var setName = SetName.Parse("v1.0:ntparc:Three-Position Air Rifle Type");
 
-            var client = new DefinitionAPIClient(xApiKey);
-
-            var response = client.GetAttributeDefinitionAsync(setName);
+            var response = _client.GetAttributeDefinitionAsync(setName);
             Assert.IsNotNull(response);
 
             var taskResult = response.Result;
-            var attribute = taskResult.Definition;
+            var objResponse = taskResult.Definition;
             var msgResponse = taskResult.MessageResponse;
 
-            Assert.IsNotNull(attribute);
+            Assert.IsNotNull(objResponse);
             Assert.IsNotNull(msgResponse);
 
-            Assert.AreEqual(attribute.SetName, setName.ToString());
-            Assert.AreEqual(attribute.Type, taskResult.DefinitionType);
-            Assert.AreEqual(attribute.Fields.Count, 1);
-            Assert.AreEqual(attribute.Fields[0].FieldName, "Three-Position Air Rifle Type");
-
+            Assert.AreEqual(objResponse.SetName, setName.ToString());
+            Assert.AreEqual(objResponse.Type, taskResult.DefinitionType);
+            Assert.AreEqual(objResponse.Fields.Count, 1);
+            Assert.AreEqual(objResponse.Fields[0].FieldName, "Three-Position Air Rifle Type");
         }
+
+        [TestMethod]
+        public void GetCourseOfFireType()
+        {
+
+            var setName = SetName.Parse("v2.0:ntparc:Three-Position Air Rifle 3x10");
+
+            var response = _client.GetCourseOfFireDefinition(setName);
+            Assert.IsNotNull(response);
+
+            var taskResult = response.Result;
+            var objResponse = taskResult.Definition;
+            var msgResponse = taskResult.MessageResponse;
+
+            Assert.IsNotNull(objResponse);
+            Assert.IsNotNull(msgResponse);
+
+            Assert.AreEqual(objResponse.SetName, setName.ToString());
+            Assert.AreEqual(objResponse.Type, taskResult.DefinitionType);
+            Assert.IsTrue(objResponse.RangeScripts.Count>=1);
+            Assert.AreEqual(objResponse.Description, setName.ProperName);
+        }
+
+        [TestMethod]
+        public void GetEventStyleType()
+        {
+
+            var setName = SetName.Parse("v1.0:ntparc:Three-Position Precision Air Rifle");
+
+            var response = _client.GetEventStyleDefinition(setName);
+            Assert.IsNotNull(response);
+
+            var taskResult = response.Result;
+            var objResponse = taskResult.Definition;
+            var msgResponse = taskResult.MessageResponse;
+
+            Assert.IsNotNull(objResponse);
+            Assert.IsNotNull(msgResponse);
+
+            Assert.AreEqual(objResponse.SetName, setName.ToString());
+            Assert.AreEqual(objResponse.Type, taskResult.DefinitionType);
+            Assert.IsTrue(objResponse.StageStyles.Count >= 1);
+            Assert.AreEqual(objResponse.Description, setName.ProperName);
+        }
+
+        [TestMethod]
+        public void GetRankingRuleType()
+        {
+
+            var setName = SetName.Parse("v1.0:nra:BB Gun Qualification");
+
+            var response = _client.GetRankingRuleDefinition(setName);
+            Assert.IsNotNull(response);
+
+            var taskResult = response.Result;
+            var objResponse = taskResult.Definition;
+            var msgResponse = taskResult.MessageResponse;
+
+            Assert.IsNotNull(objResponse);
+            Assert.IsNotNull(msgResponse);
+
+            Assert.AreEqual(objResponse.SetName, setName.ToString());
+            Assert.AreEqual(objResponse.Type, taskResult.DefinitionType);
+            Assert.IsTrue(objResponse.RankingRules.Count >= 1);
+            Assert.AreEqual(objResponse.HierarchicalName, $"{setName.Namespace}:{setName.ProperName}");
+        }
+
+        [TestMethod]
+        public void GetStageStyleType()
+        {
+
+            var setName = SetName.Parse("v1.0:ntparc:Sporter Air Rifle Standing");
+
+            var response = _client.GetStageStyleDefinition(setName);
+            Assert.IsNotNull(response);
+
+            var taskResult = response.Result;
+            var objResponse = taskResult.Definition;
+            var msgResponse = taskResult.MessageResponse;
+
+            Assert.IsNotNull(objResponse);
+            Assert.IsNotNull(msgResponse);
+
+            Assert.AreEqual(objResponse.SetName, setName.ToString());
+            Assert.AreEqual(objResponse.Type, taskResult.DefinitionType);
+            Assert.IsTrue(objResponse.DisplayScoreFormats.Count >= 1);
+            Assert.AreEqual(objResponse.Description, setName.ProperName);
+        }
+
+        [TestMethod]
+        public void GetTargetCollectionType()
+        {
+
+            var setName = SetName.Parse("v1.0:ntparc:Air Rifle");
+
+            var response = _client.GetTargetCollectionDefinition(setName);
+            Assert.IsNotNull(response);
+
+            var taskResult = response.Result;
+            var objResponse = taskResult.Definition;
+            var msgResponse = taskResult.MessageResponse;
+
+            Assert.IsNotNull(objResponse);
+            Assert.IsNotNull(msgResponse);
+
+            Assert.AreEqual(objResponse.SetName, setName.ToString());
+            Assert.AreEqual(objResponse.Type, taskResult.DefinitionType);
+            Assert.IsTrue(objResponse.TargetCollections.Count >= 1);
+            Assert.AreEqual(objResponse.HierarchicalName, $"{setName.Namespace}:{setName.ProperName}");
+        }
+
+        [TestMethod]
+        public void GetTargetType()
+        {
+
+            var setName = SetName.Parse("v1.0:issf:10m Air Rifle");
+
+            var response = _client.GetTargetDefinition(setName);
+            Assert.IsNotNull(response);
+
+            var taskResult = response.Result;
+            var objResponse = taskResult.Definition;
+            var msgResponse = taskResult.MessageResponse;
+
+            Assert.IsNotNull(objResponse);
+            Assert.IsNotNull(msgResponse);
+
+            Assert.AreEqual(objResponse.SetName, setName.ToString());
+            Assert.AreEqual(objResponse.Type, taskResult.DefinitionType);
+            Assert.IsTrue(objResponse.ScoringRings.Count >= 1);
+            Assert.AreEqual(objResponse.HierarchicalName, $"{setName.Namespace}:{setName.ProperName}");
+        }
+
+        [TestMethod]
+        public void GetScoreFormatCollectionType()
+        {
+
+            var setName = SetName.Parse("v1.0:orion:Standard Score Formats");
+
+            var response = _client.GetScoreFormatCollectionDefinition(setName);
+            Assert.IsNotNull(response);
+
+            var taskResult = response.Result;
+            var objResponse = taskResult.Definition;
+            var msgResponse = taskResult.MessageResponse;
+
+            Assert.IsNotNull(objResponse);
+            Assert.IsNotNull(msgResponse);
+
+            Assert.AreEqual(objResponse.SetName, setName.ToString());
+            Assert.AreEqual(objResponse.Type, taskResult.DefinitionType);
+            Assert.IsTrue(objResponse.ScoreFormats.Count >= 1);
+            Assert.AreEqual(objResponse.HierarchicalName, $"{setName.Namespace}:{setName.ProperName}");
+        }
+
+        //[TestMethod]
+        public void GetResultListFormatType()
+        {
+            //API fetch needs built out
+            var setName = SetName.Parse("v1.0:orion:Standard Score Formats");
+
+            var response = _client.GetResultListFormatCollection(setName);
+            Assert.IsNotNull(response);
+
+            var taskResult = response.Result;
+            var objResponse = taskResult.Definition;
+            var msgResponse = taskResult.MessageResponse;
+
+            Assert.IsNotNull(objResponse);
+            Assert.IsNotNull(msgResponse);
+
+            //Assert.AreEqual(objResponse.SetName, setName.ToString());
+            //Assert.AreEqual(objResponse.Type, taskResult.DefinitionType);
+            //Assert.IsTrue(objResponse.ScoreFormats.Count >= 1);
+            //Assert.AreEqual(objResponse.Description, setName.ProperName);
+        }
+
     }
 }
--- BabelFish/DefinitionAPIClient.cs
+++ BabelFish/DefinitionAPIClient.cs
@@ -18,23 +18,32 @@ namespace BabelFish {
         public async Task<GetDefinitionResponse<T>> GetDefinition<T>(Definition.DefinitionType type, SetName setName, GetDefinitionResponse<T> response)
         {
             /////Logic checking for caching function
-            //1. Check for local file
-            //1a. Continue API retrieval if user pref is to always pull new API
-            //1b. If !exists local file, continue API retrieval
-            //1b. If exists local file, check local file timestamp
-            //1b1. Return if within acceptable timeframe (never check for new Version??)
-            //1b2. Step2 if outside timeframe
-            //2. Exists local file + Outside Timeframe
-            //2a. API call for Version
-            //2a1. No Version returned, (ERROR instance: Return local file or continue API retrieval??)
-            //2a2. Version matches local file, return local file
-            //2a3. Version returned ! match local file, continue API retrieval
+            /// Reference https://github.com/shooterstech/BabelFish/issues/9
+            //1. Check for object in mem
+            //1a. if exists, check timestamp
+            //1a1. Return if within acceptable timeframe (never check for new Version??)
+            //1a2. Step2 if outside timeframe
+            //1b. if !exists, continue Step2
+            //2. Check for local file
+            //2a. Continue API retrieval if user pref is to always pull new API
+            //2b. If !exists local file, continue API retrieval
+            //2b. If exists local file, check local file timestamp
+            //2b1. Return if within acceptable timeframe (never check for new Version??)
+            //2b2. Step3 if outside timeframe
+            //3. Exists local file + Outside Timeframe
+            //3a. API call for Version
+            //3a1. No Version returned, (ERROR instance: Return local file or continue API retrieval??)
+            //3a2. Version matches local file, return local file
+            //3a3. Version returned ! match local file, continue API retrieval
             //if (CheckForLocalFile(setName))
             //    response = GetLocalFileObject();
-            
+
             GetDefinitionRequest requestParameters = new GetDefinitionRequest(setName, type.Description());
 
             await this.CallAPI(requestParameters, response).ConfigureAwait(false);
+
+            //Preloading: set-and-forget async thread for objects => written file -> add to mem
+            //  maybe check if user has AlwaysFetchDefinition=true and don't waste write/mem???
 
             return response;
         }
