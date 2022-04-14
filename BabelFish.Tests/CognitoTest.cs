639505875a2e57d44f7b422145bf218cf1bf5cd7
Erik Anderson <erik@shooterstech.net>
2022-04-14 10:27:43 -0400

    Added comments

--- BabelFish.Tests/CognitoTest.cs
+++ BabelFish.Tests/CognitoTest.cs
@@ -26,21 +26,49 @@ namespace BabelFish.Tests {
 
         [TestMethod]
         public async Task LoginTest() {
+
+            //See:https://docs.aws.amazon.com/sdk-for-net/v3/developer-guide/cognito-authentication-extension.html
+            //See: https://github.com/aws/aws-sdk-net-extensions-cognito#authenticating-with-secure-remote-protocol-srp
+
+            //Create anonymous credentials to create a Cognito Identify Provider
             AmazonCognitoIdentityProviderClient provider =
                 new AmazonCognitoIdentityProviderClient( new Amazon.Runtime.AnonymousAWSCredentials() );
+
+            //Set up the initial request when we don't have a refresh token. Obviously assumes an existing user.
             CognitoUserPool userPool = new CognitoUserPool( poolID, clientID, provider );
             CognitoUser user = new CognitoUser( username, clientID, userPool, provider );
+            //This is the only time we need the user's password (assuming the soon to be obtained refresh token stays valid)
             InitiateSrpAuthRequest authRequest = new InitiateSrpAuthRequest() {
                 Password = password                 
             };
 
+            //Send the request to Cognito for authentication
             AuthFlowResponse authResponse = await user.StartWithSrpAuthAsync( authRequest ).ConfigureAwait( false );
+            //TODO: Determine the response when the username/password don't match.
+
+
+            //The returned authResponse may present a second set of challenges. 
+            // * New Passwword -> RespondToNewPasswordRequiredAsync()
+            // * MFA Response -> RespondToSmsMfaAuthAsync() or RespondToMfaAuthAsyn (probable doesn't apply to us)
+
+            //Both the AccessToken (good for 60 minutes by default) and RefreshToken (good for 30 days) need to be saved
+            //DeviceKey also needs to be saved. See below, may also need to save DeviceGroupKey, and the device password.
             var accessToken = authResponse.AuthenticationResult.AccessToken;
             var refreshToken = authResponse.AuthenticationResult.RefreshToken;
-            
+            var deviceKey = authResponse.AuthenticationResult.NewDeviceMetadata.DeviceKey;
+
+            //Now we can get AWS Credentials for the user.
             CognitoAWSCredentials credentials =
                 user.GetCognitoAWSCredentials( identityPoolID, RegionEndpoint.USEast1 );
+            //TODO Using the credentials, need to obtain the Key ID and SessionToken
+            //To do so, use the .get_id() and .get_credentials_for_identity() calls
 
+            //Above, when we made the StartWithSrpAuthAsync Call, the call also generates a temporary 'Device'
+            //Now need to tell Cognito to confirm this temporary device.
+            //TODO: Determine if we will need to remember the device password.
+            //TODO: Determine how device authentication will work with users signing onto www server (instead of an App).
+
+            //Device Verification happens locally. the Salt and password verifier gets sent to Cognito in the next .ConfirmDeviceAsync() call
             var deviceVerifier = user.GenerateDeviceVerifier(
                 authResponse.AuthenticationResult.NewDeviceMetadata.DeviceGroupKey,
                 myRandomAssDevicePassword,
@@ -48,13 +76,16 @@ namespace BabelFish.Tests {
 
             ConfirmDeviceResponse confirmDeviceResponse = await user.ConfirmDeviceAsync(
                 accessToken, 
-                authResponse.AuthenticationResult.NewDeviceMetadata.DeviceKey,
+                deviceKey,
                 "Bob",
                 deviceVerifier.PasswordVerifier,
                 deviceVerifier.Salt);
 
+            //Now attach the newly attached Device to the user. We need this step to successfully complete the StartWithRefreshTokenAuthAsync() call
+            //NOTE There may be a better way to do this, but method here is to first create a generic Device, with the known 
+            //device key. Then Use GetDeviceAsync() to pull the real details from Cognito
             CognitoDevice device = new CognitoDevice(
-                authResponse.AuthenticationResult.NewDeviceMetadata.DeviceKey,
+                deviceKey,
                 new Dictionary<string, string>(),
                 DateTime.Today,
                 DateTime.Today,
@@ -62,19 +93,27 @@ namespace BabelFish.Tests {
                 user );
             await device.GetDeviceAsync();
             user.Device = device;
-            
-            
+
+            //Now pretend we need ot fast foward in time and refresh the tokens
+
+            //See: https://github.com/aws/aws-sdk-net-extensions-cognito#authenticating-using-a-refresh-token-from-a-previous-session
+
+            //I'm actaully not sure what this step does ..
             user.SessionTokens = new CognitoUserSession( null, null, refreshToken, DateTime.UtcNow, DateTime.UtcNow.AddHours( 1 ) );
 
+            //Create the refresh request object
             InitiateRefreshTokenAuthRequest refreshRequest = new InitiateRefreshTokenAuthRequest() {
                 AuthFlowType = AuthFlowType.REFRESH_TOKEN_AUTH
             };
-
-            
+                        
+            //CAll Cognito to refresh the token
             AuthFlowResponse authResponse2 = await user.StartWithRefreshTokenAuthAsync( refreshRequest ).ConfigureAwait( false );
+
+            //Now we have a new accessToken and a new refreshToken, both of which need to be re-saved
             var accessToken2 = authResponse2.AuthenticationResult.AccessToken;
             var refreshToken2 = authResponse2.AuthenticationResult.RefreshToken;
 
+            //Again, we can get new credentials for signing API requests
             CognitoAWSCredentials credentials2 =
                 user.GetCognitoAWSCredentials( identityPoolID, RegionEndpoint.USEast1 );
         }
