16a2d7e518292bc1d7abaad603f1bb795ddd9c60
Erik Anderson <erik@shooterstech.net>
2023-01-14 15:52:09 -0500

    not even sure what changed

--- BabelFish/DataModel/Clubs/ClubLicenseCapability.cs
+++ BabelFish/DataModel/Clubs/ClubLicenseCapability.cs
@@ -8,23 +8,26 @@ using Newtonsoft.Json.Converters;
 
 namespace Scopos.BabelFish.DataModel.Clubs {
 
+    /// <summary>
+    /// Medea called these LicenseFeature
+    /// </summary>
     [JsonConverter( typeof( StringEnumConverter ) )]
     public enum ClubLicenseCapability {
 
         [Description( "Allows customers to scan and score paper targets with Orion." )]
-        [EnumMember( Value = "VISSCANNER" )]
-        VISSCANNER,
+        [EnumMember( Value = "VIS_SCANNER" )]
+        VIS_SCANNER,
 
         [Description( "Allows customer to access the online Scorecard app (which is now deprecated)." )]
         [EnumMember( Value = "SCORECARD" )]
         SCORECARD,
 
         [Description( "Allows customer to use the password protected CMP upload feature." )]
-        [EnumMember( Value = "CMPUPLOAD" )]
-        CMPUPLOAD,
+        [EnumMember( Value = "CMP_CT_UPLOAD" )]
+        CMP_CT_UPLOAD,
 
         [Description( "Allows customer to access Scopos protected functionality." )]
         [EnumMember( Value = "PRIVILEGED" )]
-        PRIVILEGED 
+        PRIVILEGED
     };
 }
