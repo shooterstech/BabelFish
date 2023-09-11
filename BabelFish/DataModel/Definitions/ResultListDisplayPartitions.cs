0e65f330d464080150d308aed83609954224698f
Erik Anderson <erik@shooterstech.net>
2023-09-11 17:53:17 -0400

    Removed the default class and cell css styles on instnace creation, as this was causing problems with json deserilization

--- BabelFish/DataModel/Definitions/ResultListDisplayPartitions.cs
+++ BabelFish/DataModel/Definitions/ResultListDisplayPartitions.cs
@@ -5,12 +5,12 @@ using System.Text;
 namespace Scopos.BabelFish.DataModel.Definitions {
     public class ResultListDisplayPartitions {
 
-        public ResultListDisplayPartition Header { get; set; } = new ResultListDisplayPartition("H_Row", "H_Default");
+        public ResultListDisplayPartition Header { get; set; } = new ResultListDisplayPartition();
 
-        public ResultListDisplayPartitionExtended Body { get; set; } = new ResultListDisplayPartitionExtended( "B_Row", "B_Default" );
+        public ResultListDisplayPartitionExtended Body { get; set; } = new ResultListDisplayPartitionExtended();
 
-        public ResultListDisplayPartition Footer { get; set; } = new ResultListDisplayPartition( "F_Row", "F_Default" );
+        public ResultListDisplayPartition Footer { get; set; } = new ResultListDisplayPartition();
 
-        public ResultListDisplayPartitionExtended Children { get; set; } = new ResultListDisplayPartitionExtended( "C_Row", "C_Default" );
+        public ResultListDisplayPartitionExtended Children { get; set; } = new ResultListDisplayPartitionExtended();
     }
 }
