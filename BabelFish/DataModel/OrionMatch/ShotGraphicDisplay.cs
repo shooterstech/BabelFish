e59a602917ae84ae5db21e7f9e4cf2ea5eea68f6
Erik Anderson <erik@shooterstech.net>
2024-03-01 17:53:38 -0500

    Moved DataFormat file to under OrionMatch, as that is a better locations

---
+++ BabelFish/DataModel/OrionMatch/ShotGraphicDisplay.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Scopos.BabelFish.DataModel.OrionMatch
+{
+    /// <summary>
+    /// A ShotGraphicDisplay describes how both the target image graphic should be displayed, what shots
+    /// should be displayed on it, and the text representation of the scores for the Competitor.
+    /// </summary>
+    public class ShotGraphicDisplay
+    {
+        /// <summary>
+        /// Human Readable name used only in Post Displays
+        /// </summary>
+        public string PostDisplayName { get; set; } = String.Empty;
+
+        /// <summary>
+        /// Human readable description of what this ShotGraphicDisplay do.
+        /// </summary>
+        public string Description { get; set; } = String.Empty;
+
+        /// <summary>
+        /// Describes 1 to many target images that can be displayed, or rotated. In a Live event, only the 
+        /// first (index == 0) object should be displayed. 
+        /// </summary>
+        public List<ShotGraphicShow> Show { get; set; } = new List<ShotGraphicShow>();
+
+        /// <summary>
+        /// In a Live event, lists the current EventNames for each EventType. In a event that is
+        /// already concluded, this data structure will not have any meaning.
+        /// </summary>
+        public EventTypeNames CurrentEvents { get; set; } = new EventTypeNames();
+
+        /// <summary>
+        /// Describites what (text) scores should be displayed along witgh the target image graphics.
+        /// </summary>
+        public AbbreviatedScoreDisplay AbbreviatedFormat { get; set; } = new AbbreviatedScoreDisplay();
+    }
+}
