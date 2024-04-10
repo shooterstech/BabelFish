91202dabf264f9dddcddc5e69abc0120bef396e4
Ben Wolin <ben@shooterstech.net>
2024-04-09 23:40:22 -0400

    unit testing for coaches relationship with new CoachAssignmentCRUD

--- BabelFish.Tests/SocialNetwork/RelationshipRoleCRADTests.cs
+++ BabelFish.Tests/SocialNetwork/RelationshipRoleCRADTests.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Scopos.BabelFish.APIClients;
 using Scopos.BabelFish.DataModel.SocialNetwork;
+using Scopos.BabelFish.Requests.ClubsAPI;
 using Scopos.BabelFish.Requests.SocialNetworkAPI;
 using Scopos.BabelFish.Runtime.Authentication;
 using System;
@@ -15,11 +16,12 @@ namespace Scopos.BabelFish.Tests.SocialNetwork
     public class RelationshipRoleCRADTests
     {
         private SocialNetworkAPIClient socialNetworkClient;
+        private ClubsAPIClient clubsClient; //required to test coach relationship/ designate coaches
 
-        
         [TestInitialize] 
         public void InitClient() {
-            socialNetworkClient = new SocialNetworkAPIClient(Constants.X_API_KEY, APIStage.BETA);
+            socialNetworkClient = new SocialNetworkAPIClient(Constants.X_API_KEY, APIStage.ALPHA);
+            clubsClient = new ClubsAPIClient(Constants.X_API_KEY, APIStage.ALPHA);
         }
 
         [TestMethod]
@@ -445,8 +447,194 @@ namespace Scopos.BabelFish.Tests.SocialNetwork
         }
 
         [TestMethod]
+        public async Task TestCoachApprovalErrors()
+        {
+            /* In this test case, User1 is a POC for Orion license 7 (valid) and 2 (expired)
+             *  User 3 is acting as a coach
+             *  User 7 is acting as an athlete
+             *  
+             *  TODO: How to unit test license expiring?
+             *  - Cant assign a coach to expired license through CoachAssignmentCRUD
+             */
+            var pocAuthentication = new UserAuthentication( //license 7 POC
+                Constants.TestDev1Credentials.Username,
+                Constants.TestDev1Credentials.Password);
+            await pocAuthentication.InitializeAsync();
+
+            var athleteAuthentication = new UserAuthentication(
+                Constants.TestDev7Credentials.Username,
+                Constants.TestDev7Credentials.Password);
+            await athleteAuthentication.InitializeAsync();
+            var athleteUserId = Constants.TestDev7UserId;
+
+            var coachAuthentication = new UserAuthentication(
+                Constants.TestDev3Credentials.Username,
+                Constants.TestDev3Credentials.Password);
+            await coachAuthentication.InitializeAsync();
+            var coachUserId = Constants.TestDev3UserId;
+
+            int validLicense = 7;
+            //int expiredLicense = 2; //cant assign a coach to expired license through CoachAssignmentCRUD
+
+            //user 3 should not be a designated coach to start out
+            var coachDeleteReq = new DeleteCoachAssignmentAuthenticatedRequest(pocAuthentication);
+            coachDeleteReq.LicenseNumber = validLicense;
+            coachDeleteReq.UserId.Add(coachUserId);
+            var coachDeleteResp = await clubsClient.DeleteCoachAssignmentAuthenticatedAsync(coachDeleteReq);
+            Assert.AreEqual(System.Net.HttpStatusCode.OK, coachDeleteResp.StatusCode);
+            //coachDeleteReq.LicenseNumber = expiredLicense;
+            //coachDeleteResp = await clubsClient.DeleteCoachAssignmentAuthenticatedAsync(coachDeleteReq);
+            //Assert.AreEqual(System.Net.HttpStatusCode.OK, coachDeleteResp.StatusCode);
+
+            //ensure role doesnt exist already
+            var deleteRequest = new DeleteRelationshipRoleAuthenticatedRequest(athleteAuthentication);
+            deleteRequest.RelationshipName = SocialRelationshipName.COACH;
+            deleteRequest.ActiveId = coachUserId;
+            var deleteResponse = await socialNetworkClient.DeleteRelationshipRoleAuthenticatedAsync(deleteRequest);
+            Assert.IsTrue(System.Net.HttpStatusCode.OK == deleteResponse.StatusCode || System.Net.HttpStatusCode.NotFound == deleteResponse.StatusCode);
+
+            //unauthorized coach requests to coach an athlete
+            var createRequestFromCoach = new CreateRelationshipRoleAuthenticatedRequest(coachAuthentication);
+            createRequestFromCoach.RelationshipName = SocialRelationshipName.COACH;
+            createRequestFromCoach.ActiveId = coachUserId;
+            createRequestFromCoach.PassiveId = athleteUserId;
+            var createResponse = await socialNetworkClient.CreateRelationshipRoleAuthenticatedAsync(createRequestFromCoach);
+            Assert.AreEqual(System.Net.HttpStatusCode.Unauthorized, createResponse.StatusCode);
+
+            //athlete requests to be coached by unauthorized coach 
+            var createRequestFromAthlete = new CreateRelationshipRoleAuthenticatedRequest(athleteAuthentication);
+            createRequestFromAthlete.RelationshipName = SocialRelationshipName.COACH;
+            createRequestFromAthlete.ActiveId = coachUserId;
+            createRequestFromAthlete.PassiveId = athleteUserId;
+            createResponse = await socialNetworkClient.CreateRelationshipRoleAuthenticatedAsync(createRequestFromAthlete);
+            Assert.AreEqual(System.Net.HttpStatusCode.Unauthorized, createResponse.StatusCode);
+
+            //add coach to expired license
+            //var coachCreateReq = new CreateCoachAssignmentAuthenticatedRequest(pocAuthentication);
+            //coachCreateReq.LicenseNumber = expiredLicense;
+            //coachCreateReq.UserId.Add(coachUserId);
+            //var coachCreateResp = await clubsClient.CreateCoachAssignmentAuthenticatedAsync(coachCreateReq);
+            //Assert.AreEqual(System.Net.HttpStatusCode.OK, coachCreateResp.StatusCode);
+
+            ////expired coach requests to coach an athlete
+            //createResponse = await socialNetworkClient.CreateRelationshipRoleAuthenticatedAsync(createRequestFromCoach);
+            //Assert.AreEqual(System.Net.HttpStatusCode.Unauthorized, createResponse.StatusCode);
+
+            ////athlete requests to be coached by expired coach
+            //createResponse = await socialNetworkClient.CreateRelationshipRoleAuthenticatedAsync(createRequestFromAthlete);
+            //Assert.AreEqual(System.Net.HttpStatusCode.Unauthorized, createResponse.StatusCode);
+
+            //add coach to valid license (still attached to expired license)
+            var coachCreateReq = new CreateCoachAssignmentAuthenticatedRequest(pocAuthentication);
+            coachCreateReq.UserId.Add(coachUserId);
+            coachCreateReq.LicenseNumber = validLicense;
+            var coachCreateResp = await clubsClient.CreateCoachAssignmentAuthenticatedAsync(coachCreateReq);
+            Assert.AreEqual(System.Net.HttpStatusCode.OK, coachCreateResp.StatusCode);
+
+            //BEGIN COACH REQUEST ATHLETE TESTS
+
+            //valid coach requests athlete
+            createResponse = await socialNetworkClient.CreateRelationshipRoleAuthenticatedAsync(createRequestFromCoach);
+            Assert.AreEqual(System.Net.HttpStatusCode.OK, createResponse.StatusCode);
+
+            //coach is removed from valid license (now unauthorized) //(all licenses now expired)
+            coachDeleteReq.LicenseNumber = validLicense; 
+            coachDeleteResp = await clubsClient.DeleteCoachAssignmentAuthenticatedAsync(coachDeleteReq);
+            Assert.AreEqual(System.Net.HttpStatusCode.OK, coachDeleteResp.StatusCode);
+
+            //athlete attempts to accept coah request from expired coach
+            //var approveRequestFromAthlete = new ApproveRelationshipRoleAuthenticatedRequest(athleteAuthentication);
+            //approveRequestFromAthlete.RelationshipName = SocialRelationshipName.COACH;
+            //approveRequestFromAthlete.ActiveId = coachUserId;
+            //approveRequestFromAthlete.PassiveId = athleteUserId;
+            //var approveResponse = await socialNetworkClient.ApproveRelationshipRoleAuthenticatedAsync(approveRequestFromAthlete);
+            //Assert.AreEqual(System.Net.HttpStatusCode.Unauthorized, approveResponse.StatusCode);
+
+            ////expired license removed from coach (coach is now not designated under any license)
+            //coachDeleteReq.LicenseNumber = expiredLicense;
+            //coachDeleteResp = await clubsClient.DeleteCoachAssignmentAuthenticatedAsync(coachDeleteReq);
+            //Assert.AreEqual(System.Net.HttpStatusCode.OK, coachDeleteResp.StatusCode);
+
+            //athlete attempts to accept coach request from unauthorized coach
+            var approveRequestFromAthlete = new ApproveRelationshipRoleAuthenticatedRequest(athleteAuthentication);
+            approveRequestFromAthlete.RelationshipName = SocialRelationshipName.COACH;
+            approveRequestFromAthlete.ActiveId = coachUserId;
+            approveRequestFromAthlete.PassiveId = athleteUserId;
+            var approveResponse = await socialNetworkClient.ApproveRelationshipRoleAuthenticatedAsync(approveRequestFromAthlete);
+            Assert.AreEqual(System.Net.HttpStatusCode.Unauthorized, approveResponse.StatusCode);
+
+            //athlete deletes request
+            deleteResponse = await socialNetworkClient.DeleteRelationshipRoleAuthenticatedAsync(deleteRequest);
+            Assert.IsTrue(System.Net.HttpStatusCode.OK == deleteResponse.StatusCode);
+
+            //BEGIN ATHLETE REQUEST COACH TESTS
+
+            //Add coach to valid license
+            coachCreateReq.LicenseNumber = validLicense;
+            coachCreateResp = await clubsClient.CreateCoachAssignmentAuthenticatedAsync(coachCreateReq);
+            Assert.IsTrue(System.Net.HttpStatusCode.OK == coachCreateResp.StatusCode);
+
+            //athlete requests to coach a valid coach (athlete is not a valid coach)
+            createRequestFromAthlete.ActiveId = athleteUserId;
+            createRequestFromAthlete.PassiveId = coachUserId;
+            createResponse = await socialNetworkClient.CreateRelationshipRoleAuthenticatedAsync(createRequestFromAthlete);
+            Assert.AreEqual(System.Net.HttpStatusCode.Unauthorized, createResponse.StatusCode);
+
+            //athlete requests valid coach
+            createRequestFromAthlete.ActiveId = coachUserId;
+            createRequestFromAthlete.PassiveId = athleteUserId;
+            createResponse = await socialNetworkClient.CreateRelationshipRoleAuthenticatedAsync(createRequestFromAthlete);
+            Assert.AreEqual(System.Net.HttpStatusCode.OK, createResponse.StatusCode);
+
+            //add coach to expired license
+            //coachCreateReq.LicenseNumber = expiredLicense;
+            //coachCreateResp = await clubsClient.CreateCoachAssignmentAuthenticatedAsync(coachCreateReq);
+            //Assert.IsTrue(System.Net.HttpStatusCode.OK == coachCreateResp.StatusCode);
+
+            //coach is removed from valid license (now unauthorized) //(all licenses now expired)
+            coachDeleteReq.LicenseNumber = validLicense;
+            coachDeleteResp = await clubsClient.DeleteCoachAssignmentAuthenticatedAsync(coachDeleteReq);
+            Assert.AreEqual(System.Net.HttpStatusCode.OK, coachDeleteResp.StatusCode);
+
+            ////expired coach attempts to accept athlete request
+            //var approveRequestFromCoach = new ApproveRelationshipRoleAuthenticatedRequest(coachAuthentication);
+            //approveRequestFromCoach.RelationshipName = SocialRelationshipName.COACH;
+            //approveRequestFromCoach.ActiveId = coachUserId;
+            //approveRequestFromCoach.PassiveId = athleteUserId;
+            //approveResponse = await socialNetworkClient.ApproveRelationshipRoleAuthenticatedAsync(approveRequestFromCoach);
+            //Assert.AreEqual(System.Net.HttpStatusCode.Unauthorized, approveResponse.StatusCode);
+
+            ////expired license removed from expired license (coach is now not designated under any license)
+            //coachDeleteReq.LicenseNumber = expiredLicense;
+            //coachDeleteResp = await clubsClient.DeleteCoachAssignmentAuthenticatedAsync(coachDeleteReq);
+            //Assert.AreEqual(System.Net.HttpStatusCode.OK, coachDeleteResp.StatusCode);
+
+            //unauthorized coach attempts to accept athlete request
+            var approveRequestFromCoach = new ApproveRelationshipRoleAuthenticatedRequest(coachAuthentication);
+            approveRequestFromCoach.RelationshipName = SocialRelationshipName.COACH;
+            approveRequestFromCoach.ActiveId = coachUserId;
+            approveRequestFromCoach.PassiveId = athleteUserId;
+            approveResponse = await socialNetworkClient.ApproveRelationshipRoleAuthenticatedAsync(approveRequestFromCoach);
+            Assert.AreEqual(System.Net.HttpStatusCode.Unauthorized, approveResponse.StatusCode);
+
+            //athlete deletes request
+            deleteResponse = await socialNetworkClient.DeleteRelationshipRoleAuthenticatedAsync(deleteRequest);
+            Assert.IsTrue(System.Net.HttpStatusCode.OK == deleteResponse.StatusCode);
+
+
+
+            //TODO(?): should coach relationship still be able to be read if coach is no longer valid?
+
+        }
+
+        [TestMethod]
         public async Task TestCoachApprovalProcess()
         {
+            var user1Authentication = new UserAuthentication( //license 7 POC
+                Constants.TestDev1Credentials.Username,
+                Constants.TestDev1Credentials.Password);
+            await user1Authentication.InitializeAsync();
+            int licenseNumber = 7;
 
             var user7Authentication = new UserAuthentication(
                 Constants.TestDev7Credentials.Username,
@@ -458,6 +646,14 @@ namespace Scopos.BabelFish.Tests.SocialNetwork
                 Constants.TestDev3Credentials.Password);
             await user3Authentication.InitializeAsync();
 
+            
+            //ensure user3 is designated coach
+            var createCoachReq = new CreateCoachAssignmentAuthenticatedRequest(user1Authentication);
+            createCoachReq.LicenseNumber = licenseNumber;
+            createCoachReq.UserId.Add(Constants.TestDev3UserId);
+            var createCoachResp = await clubsClient.CreateCoachAssignmentAuthenticatedAsync(createCoachReq);
+            Assert.AreEqual(System.Net.HttpStatusCode.OK, createCoachResp.StatusCode);
+
             //ensure role doesnt exist already
             var deleteRequest = new DeleteRelationshipRoleAuthenticatedRequest(user7Authentication);
             deleteRequest.RelationshipName = SocialRelationshipName.COACH;
@@ -467,7 +663,8 @@ namespace Scopos.BabelFish.Tests.SocialNetwork
 
             Assert.IsTrue(System.Net.HttpStatusCode.OK == deleteResponse.StatusCode || System.Net.HttpStatusCode.NotFound == deleteResponse.StatusCode);
 
-            //user7 requests that user3 coaches them
+
+            //athlete (user 7) requests that user3 coaches them
             var createRequest = new CreateRelationshipRoleAuthenticatedRequest(user7Authentication);
             createRequest.RelationshipName = SocialRelationshipName.COACH;
             createRequest.ActiveId = Constants.TestDev3UserId;
