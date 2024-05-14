aabf6637a0bf505b7c89cfde5547acab63d7c335
LiamEberhart <liam@shooterstech.net>
2024-05-14 14:44:57 -0400

    passing tests, doing happy things

--- BabelFish.Tests/OrionMatch/AvgPredictionsTests.cs
+++ BabelFish.Tests/OrionMatch/AvgPredictionsTests.cs
@@ -5,6 +5,9 @@ using Scopos.BabelFish.APIClients;
 using Scopos.BabelFish.DataModel.OrionMatch;
 using Scopos.BabelFish.DataActors.OrionMatch;
 using Amazon.DynamoDBv2.Model;
+using Scopos.BabelFish.DataModel.Definitions;
+using System.Collections.Generic;
+using System;
 
 
 namespace Scopos.BabelFish.Tests.OrionMatch
@@ -13,22 +16,119 @@ namespace Scopos.BabelFish.Tests.OrionMatch
     [TestClass]
     public class AvgPredictionsTests
     {
+        public CourseOfFire courseOfFire = new CourseOfFire();
+        public ResultEvent resultEvent = new ResultEvent();
 
-        // might should just use an API call, might be easier....
+        public void CreateSampleResultEvent()
+        {
+            //need to figure out how to add eventScores.
+            resultEvent.EventScores = new Dictionary<string, EventScore>();
+            EventScore qually = new EventScore
+            {
+                EventName = "Qualification",
+                Score = new DataModel.Athena.Score { I = 99, D = 101.4f, X = 4 },
+                ScoreFormatted = "",
+                NumShotsFired = 10,
+                EventType = "EVENT"
+            };
+            resultEvent.EventScores.Add("Qualification", qually);
+            EventScore standing = new EventScore
+            {
+                EventName = "Standing",
+                Score = new DataModel.Athena.Score { I = 99, D = 101.4f, X = 4 },
+                ScoreFormatted = "",
+                NumShotsFired = 10,
+                EventType = "STAGE"
+            };
+            resultEvent.EventScores.Add("Standing", standing);
+
+
+            EventScore prone = new EventScore
+            {
+                EventName = "Prone",
+                Score = new DataModel.Athena.Score { I = 0, D = 0, X = 0 },
+                ScoreFormatted = "",
+                NumShotsFired = 0,
+                EventType = "STAGE"
+            };
+            resultEvent.EventScores.Add("Prone", prone);
+        }
+
+        public void CreateSampleCOF()
+        {
+            courseOfFire.Singulars = new List<Singular>();
+            courseOfFire.Events = new List<Event>();
+
+            Singular blah = new Singular
+            {
+                Values = "1..20",
+                EventName = "S{}",
+                ScoreFormat = "Shots",
+                StageLabel = "S",
+                Type = SingularType.SHOT
+            };
+            Singular nah = new Singular();
+            nah.Values = "1..20";
+            nah.EventName = "P{}";
+            nah.ScoreFormat = "Shots";
+            nah.StageLabel = "P";
+            nah.Type = SingularType.SHOT;
+            courseOfFire.Singulars.Add(nah);
+            courseOfFire.Singulars.Add( blah );
+
+            Event @event = new Event();
+            @event.EventName = "Qualification";
+            @event.EventType = EventtType.EVENT;
+            @event.Children = new List<string>() { "Standing","Prone" };
+            @event.Calculation = "SUM";
+            @event.ScoreFormat = "Events";
+
+            Event @stage1 = new Event();
+            @stage1.EventName = "Prone";
+            @stage1.EventType = EventtType.STAGE;
+            var dict1 = new Dictionary<string, string>
+            {
+                { "EventName", "P{}" },
+                { "Values", "1..20" }
+            };
+            @stage1.Children = dict1;
+            @stage1.Calculation = "SUM";
+            @stage1.ScoreFormat = "Events";
+
+            Event @stage2 = new Event();
+            @stage2.EventName = "Standing";
+            @stage2.EventType = EventtType.STAGE;
+            var dict2 = new Dictionary<string, string>
+            {
+                { "EventName", "S{}" },
+                { "Values", "1..20" }
+            };
+            @stage2.Children = dict2;
+            @stage2.Calculation = "SUM";
+            @stage2.ScoreFormat = "Events";
+
+            courseOfFire.Events.Add(@event);
+            courseOfFire.Events.Add(@stage1);
+            courseOfFire.Events.Add(@stage2);
+        }
+
         [TestMethod]
         public async Task ProjectedAvgScoresMakerTests()
         {
-            var client = new OrionMatchAPIClient(Constants.X_API_KEY, APIStage.BETA);
+            CreateSampleCOF();
+            CreateSampleResultEvent();
 
-            string resultCofId = "4608b306-8b6d-40c2-b608-e5375d05bd12";
-
-            var response = await client.GetResultCourseOfFireDetailPublicAsync(resultCofId);
-            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
-
-            //This result cof should of 30 shots, and 8 event scores
-            ResultCOF resultCof = response.ResultCOF;
-
-            object test = ProjectScoresByAverageShotFired();
+            resultEvent.ProjectScores(new ProjectScoresByAverageShotFired(courseOfFire));
+            Console.WriteLine(System.Text.Json.JsonSerializer.Serialize(resultEvent));
+            Assert.AreEqual(resultEvent.EventScores["Qualification"].Projected.I, 396);
+            Assert.AreEqual(resultEvent.EventScores["Qualification"].Projected.D, 405.6f);
+            Assert.AreEqual(resultEvent.EventScores["Qualification"].Projected.X, 16);
+            Assert.AreEqual(resultEvent.EventScores["Standing"].Projected.I, 198);
+            Assert.AreEqual(resultEvent.EventScores["Standing"].Projected.D, 202.8f);
+            Assert.AreEqual(resultEvent.EventScores["Standing"].Projected.X, 8);
+            Assert.AreEqual(resultEvent.EventScores["Prone"].Projected.I, 198);
+            Assert.AreEqual(resultEvent.EventScores["Prone"].Projected.D, 202.8f);
+            Assert.AreEqual(resultEvent.EventScores["Prone"].Projected.X, 8);
         }
     }
 }
--- BabelFish/DataActors/OrionMatch/ProjectScoresByAverageShotFired.cs
+++ BabelFish/DataActors/OrionMatch/ProjectScoresByAverageShotFired.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Amazon.Runtime.Internal.Transform;
+using Scopos.BabelFish.DataModel.AthenaLogin;
 using Scopos.BabelFish.DataModel.Definitions;
 using Scopos.BabelFish.DataModel.OrionMatch;
 
@@ -22,22 +24,53 @@ namespace Scopos.BabelFish.DataActors.OrionMatch {
             projection.EventScores.TryGetValue(topLevelEvent.EventName, out topLevelEventScore);
             int shotsInEvent = 0;
 
-            foreach (var stageEvent in stageStyleEvents) {
-                EventScore es;
+            EventScore es;
+            //first key is eventName, second key is avgType [(I)INT, (D)DEC, (X)INNER] value is avg.
+            Dictionary<string, Dictionary<string, float>> avgShots = new Dictionary<string, Dictionary<string, float>>();
+
+            //I could probably just use singulars and get the complete avg shot fired and use that instead, might be easier...
+            foreach (var stageEvent in stageStyleEvents)
+            {
                 projection.EventScores.TryGetValue(stageEvent.EventName, out es);
                 //get singulars for stage I am in, then count those and that is how many shots to take total.
                 var singulars = stageEvent.GetEvents(false, false, false, false, false, true);
                 var shotsFired = es.NumShotsFired;
+                //we want to always project shots if we have ANY remaining.
                 var shotsRemaining = singulars.Count - shotsFired;
-                var avgShotThisStage = es.Score.D / shotsFired;
+                var avgIntThisStage = 0.0f;
+                var avgDecThisStage = 0.0f;
+                var avgXPerShot = 0.0f;
+                if (shotsFired > 0 && singulars.Count > 0)
+                { //if there are shots fired in this stage, and there should be, we should use those 
+                    avgIntThisStage = (float)es.Score.I / (float)shotsFired;
+                    avgDecThisStage = es.Score.D / shotsFired;
+                    avgXPerShot = (float)es.Score.X / (float)shotsFired;
+
+                    shotsInEvent += singulars.Count;
+                    avgShots.Add(stageEvent.EventName, new Dictionary<string, float>());
+                    avgShots[stageEvent.EventName].Add("I", avgIntThisStage);
+                    avgShots[stageEvent.EventName].Add("D", avgDecThisStage);
+                    avgShots[stageEvent.EventName].Add("X", avgXPerShot);
+                }
+                else if(avgShots.Count > 0 && shotsFired == 0)
+                {//if we have avg from another stage and there arent shots fired in this stage yet
+                    //use the avg of the avgs
+                    var defaulting = GetAverageShot(avgShots);
+                    avgIntThisStage = defaulting["I"];
+                    avgDecThisStage = defaulting["D"];
+                    avgXPerShot = defaulting["X"];
+                    //add this to the avgShots dict for this stage.
+                    avgShots.Add(stageEvent.EventName, new Dictionary<string, float>());
+                    avgShots[stageEvent.EventName].Add("I", avgIntThisStage);
+                    avgShots[stageEvent.EventName].Add("D", avgDecThisStage);
+                    avgShots[stageEvent.EventName].Add("X", avgXPerShot);
+                }
 
                 //project the scores
                 es.Projected = new DataModel.Athena.Score();
-                es.Projected.X = (int)(es.Score.X + ((es.Score.X / shotsFired) * shotsRemaining));
-                es.Projected.I = (int)(es.Score.I + (int)(avgShotThisStage * shotsRemaining));
-                es.Projected.D = (1.0f) * (es.Score.D + (avgShotThisStage * shotsRemaining));
-
-                shotsInEvent += singulars.Count;
+                es.Projected.I = es.Score.I + (int)(avgIntThisStage * shotsRemaining);
+                es.Projected.D = es.Score.D + (avgDecThisStage * shotsRemaining);
+                es.Projected.X = (int)(es.Score.X + (avgXPerShot * shotsRemaining));
             }
 
             //Make the bold and wildly incorrect assumption that the topLevelEvent is the sum of the stages
@@ -45,15 +78,34 @@ namespace Scopos.BabelFish.DataActors.OrionMatch {
             //Need to instantiate .Projected as it is by default null
             topLevelEventScore.Projected = new DataModel.Athena.Score();
 
-            foreach (var stageEvent in stageStyleEvents) {
-                EventScore es;
+            foreach (var stageEvent in stageStyleEvents)
+            {
                 projection.EventScores.TryGetValue(stageEvent.EventName, out es);
-
-
-                topLevelEventScore.Projected.X = es.Projected.X;
-                topLevelEventScore.Projected.I = es.Projected.I;
-                topLevelEventScore.Projected.D = es.Projected.D;
+                //Add all the stage projected scores together, this will be the top level event, hopefully.
+                topLevelEventScore.Projected.I += es.Projected.I;
+                topLevelEventScore.Projected.D += es.Projected.D;
+                topLevelEventScore.Projected.X += es.Projected.X;
             }
+        }
+
+        private Dictionary<string, float> GetAverageShot(Dictionary<string,Dictionary<string,float>> allAvgs )
+        {
+            var avgInt = 0.0f;
+            var avgDec = 0.0f;
+            var avgX = 0.0f;
+            foreach (var @event in allAvgs)
+            {
+                avgInt += @event.Value["I"];
+                avgDec += @event.Value["D"];
+                avgX += @event.Value["X"];
+            }
+            Dictionary<string, float> bigAvgs = new Dictionary<string, float>
+            {
+                {"I", avgInt/(float)allAvgs.Count},
+                {"D", avgDec/(float)allAvgs.Count},
+                {"X", avgX/(float)allAvgs.Count},
+            };
+            return bigAvgs;
         }
     }
 }
