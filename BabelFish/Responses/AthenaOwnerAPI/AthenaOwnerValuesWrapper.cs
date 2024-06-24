88eaddca83263ebcb555a0f98bf20c015a499bc6
Ben Wolin <ben@shooterstech.net>
2024-06-24 14:26:13 -0400

    athena owner api requests and API client

---
+++ BabelFish/Responses/AthenaOwnerAPI/AthenaOwnerValuesWrapper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Scopos.BabelFish.DataModel.AthenaOwner;
+
+namespace Scopos.BabelFish.Responses.AthenaOwnerAPI
+{
+    public class AthenaOwnerValuesWrapper : BaseClass
+    {
+
+        public AthenaOwnerValues AthenaOwnerValues { get; set; }
+    }
+}
