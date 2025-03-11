df642f5f5b1044a24974bb373ae94ec9344bca4b
Erik Anderson <erik@shooterstech.net>
2025-03-11 18:00:08 -0400

    Added interfaces for retreiving target definitions and rankingrule definitions

---
+++ BabelFish/DataModel/Definitions/IGetAttributeDefinition.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Scopos.BabelFish.DataModel.Definitions {
+
+    /// <summary>
+    /// Classes that reference Attribute Definitions should implement this interface.
+    /// </summary>
+    public interface IGetAttributeDefinition {
+
+        /// <summary>
+        /// Retreives the Attribute Definition referenced by the instantiating class.
+        /// </summary>
+        /// <returns></returns>
+		Task<Attribute> GetAttributeDefinitionAsync();
+    }
+}
