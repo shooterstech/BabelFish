e5022791da11ac770f17b893e73659d3933be561
Ben Wolin <ben@shooterstech.net>
2025-08-18 16:40:09 -0400

    GetPressReleaseAuthenticated working

---
+++ BabelFish/Responses/OrionMatchAPI/GetPressReleaseGenerationAuthenticatedResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Scopos.BabelFish.DataModel.OrionMatch;
+using Scopos.BabelFish.Requests.OrionMatchAPI;
+
+namespace Scopos.BabelFish.Responses.OrionMatchAPI
+{
+	public  class GetPressReleaseGenerationAuthenticatedResponse: Response<PressReleaseGenerationResponseWrapper>
+	{
+		public GetPressReleaseGenerationAuthenticatedResponse(GetPressReleaseGenerationAuthenticatedRequest request)
+		{
+			this.Request = request;	
+		}
+
+		public PressReleaseGeneration PressReleaseGeneration => Value.PressReleaseGeneration;
+	}
+}
