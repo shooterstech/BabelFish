849f2a51a19d34babe780d7752c232f2429bc693
LiamEberhart <liam@shooterstech.net>
2025-10-02 18:09:36 -0400

    there's the file too!

---
+++ BabelFish/DataActors/Excel/ExcelGenerator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OfficeOpenXml;
+using OfficeOpenXml.Style;
+
+namespace Scopos.BabelFish.DataActors.Excel
+{
+    public abstract class ExcelGenerator
+    {
+        public abstract ExcelPackage GenerateExcel(List<string> data, string filePath);
+    }
+}
