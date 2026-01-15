b0c23132c44fee18e76d13b7e1e775bfc5fcb03a
Erik Anderson <erik@shooterstech.net>
2026-01-15 17:34:46 -0500

    Updating xml documentation

---
+++ src/BabelFish/APIClients/APIClientNamespaceGroupDoc.cs
@@ -0,0 +1,8 @@
+namespace Scopos.BabelFish.APIClients {
+    /// <summary>
+    /// Contains a set of facade classes that call Scopos' REST API. Classes are named based on the data models they interact with.
+    /// <para>Calling these classes require an x-api-key. To learn how to obtain an x-api-key for your application visit <see href="https://support.scopos.tech/index.html?x-api-key.html">support.scopos.tech</see>.</para>
+    /// </summary>
+    public class NamespaceGroupDoc {
+    }
+}
