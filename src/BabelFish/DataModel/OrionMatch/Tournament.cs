aef68ff0c30a06e090e56127bd199ac9c34b8c52
Ben Wolin <ben@shooterstech.net>
2026-04-15 17:05:28 -0400

    Delete MatchBase, move existing fields to Tournament

--- src/BabelFish/DataModel/OrionMatch/Tournament.cs
+++ src/BabelFish/DataModel/OrionMatch/Tournament.cs
@@ -1,6 +1,8 @@
 using Scopos.BabelFish.APIClients;
 using Scopos.BabelFish.DataActors.OrionMatch;
 using Scopos.BabelFish.DataActors.ResultListMerger;
+using Scopos.BabelFish.DataModel.Common;
+
 
 namespace Scopos.BabelFish.DataModel.OrionMatch {
 
@@ -11,7 +13,7 @@ namespace Scopos.BabelFish.DataModel.OrionMatch {
     /// These are called <see cref="MergedResultList"/>.</para>
     /// <para>The <see cref="MatchID"/> for tournaments always end in ".2".</para>
     /// </summary>
-    public class Tournament : MatchBase,
+    public class Tournament : 
         IMergedResultListContainer,
         G_STJ_SER.IJsonOnDeserialized,
         G_STJ_SER.IJsonOnDeserializing {
@@ -28,7 +30,7 @@ namespace Scopos.BabelFish.DataModel.OrionMatch {
         /// <para>Usually not called directly, instead Tournaments are read from REST API using
         /// <see cref="OrionMatchAPIClient.GetTournamentPublicAsync(MatchID)"/> (or related method).</para>
         /// </summary>
-        public Tournament() : base() { }
+        public Tournament()  { }
 
         /// <summary>
         /// This method is called after deserialization with System.Text.Json. 
@@ -70,6 +72,88 @@ namespace Scopos.BabelFish.DataModel.OrionMatch {
         #endregion
 
         #region Data Model Properties
+        /// <summary>
+        /// Concrete class identifier
+        /// </summary>
+        [G_NS.JsonProperty( Order = 1, DefaultValueHandling = Newtonsoft.Json.DefaultValueHandling.Include )]
+        public MatchType MatchType { get; set; }
+
+        /// <summary>
+        /// The Unique Match ID
+        /// </summary>
+        [G_NS.JsonProperty( Order = 2 )]
+        [G_NS.JsonConverter( typeof( G_BF_NS_CONV.MatchIdConverter ) )]
+        [G_STJ_SER.JsonConverter( typeof( G_BF_STJ_CONV.MatchIdConverter ) )]
+        public MatchID MatchId { get; set; }
+
+        /* Order = 3 reserved for concrete classes */
+
+        /// <summary>
+        /// Human readable name given to the match. Does not have to be unique (but is helpful if it is).
+        /// </summary>
+        [G_NS.JsonProperty( Order = 4 )]
+        public string MatchName { get; set; }
+
+        //EKA Question Oct 2025: Should we have a ShortMatchName. The idea is, if this match is part of a Tournament, this ShortMatchName can be used as the default column header name.
+
+        /* Order = 5 reserved for concrete classes */
+
+        /// <summary>
+        /// The account identifier that owns this Match.
+        /// <para>Will be in the form OrionAcct000001 or AtHome000001.</para>
+        /// </summary>
+        [G_NS.JsonProperty( Order = 6 )]
+        public string OwnerId { get; set; } = string.Empty;
+
+        /* Order = 7, 8, 9 skipped for future use */
+
+        /// <summary>
+        /// The start date of the match
+        /// </summary>
+        /// <example>2001-01-01</example>
+        [G_STJ_SER.JsonConverter( typeof( G_BF_STJ_CONV.ScoposDateOnlyConverter ) )]
+        [G_NS.JsonConverter( typeof( G_BF_NS_CONV.DateConverter ) )]
+        [G_NS.JsonProperty( Order = 10 )]
+        public DateTime StartDate { get; set; } = DateTime.Today;
+
+        /// <summary>
+        /// The end date of the match
+        /// </summary>
+        /// <example>2001-01-01</example>
+        [G_STJ_SER.JsonConverter( typeof( G_BF_STJ_CONV.ScoposDateOnlyConverter ) )]
+        [G_NS.JsonConverter( typeof( G_BF_NS_CONV.DateConverter ) )]
+        [G_NS.JsonProperty( Order = 11 )]
+        public DateTime EndDate { get; set; } = DateTime.Today;
+
+        /// <summary>
+        /// The visibility (who can see it) given to this match. 
+        /// </summary>
+        [G_NS.JsonProperty( Order = 12, DefaultValueHandling = Newtonsoft.Json.DefaultValueHandling.Include )]
+        public VisibilityOption Visibility { get; set; } = VisibilityOption.PRIVATE;
+
+        /* Order = 13, 14 skipped for future use */
+
+        /// <summary>
+        /// Indicates if this match should be include on match search results.
+        /// </summary>
+        [G_NS.JsonProperty( Order = 15 )]
+        public bool IncludeInSearchResults { get; set; } = false;
+
+        /// <summary>
+        /// List of officials for this match.
+        /// </summary>
+        /// <remarks>OCT 2025: Currently unknown what this object will look like.</remarks>
+        public List<object> Officials { get; set; } = new List<object>();
+
+        /// <summary>
+        /// Newtonsoft helper method to determine if Officials should be part of the serialized json.
+        /// </summary>
+        /// <returns></returns>
+        public bool ShouldSerializeOfficials() {
+            return Officials != null && Officials.Count > 0;
+        }
+
+
         /// <summary>
         /// Purposefully redundant. Exact same value as MatchName. But since this is a concrete 
         /// implementation of a Tournament we call it TournamentName.
